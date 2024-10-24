00e727595f53dadab07a975191e5c0a3265dacf6
kimjiwook0129 <kimjiwook0129@gmail.com>
2024-10-24 14:57:14 -0400

    updated

--- app.py
+++ app.py
@@ -31,5 +31,10 @@ if st.button("요청"):
     elif not image:
         st.error("사진을 업로드해주세요.")
     else:
-        st.markdown(f"""{generate_photo_title(image, instruction)}""")
+        try:
+            st.markdown(f"""{generate_photo_title(image, instruction)}""")
+        except:
+            st.error("챗지피티에 문제가 있습니다. 관리자에게 연락해주세요.")
+
+
         
